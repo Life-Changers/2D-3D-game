feccfd4bef5ac9f11852e365d60209cca2432c49
hristo10481 <hristokerezov@yahoo.com>
2022-01-25 17:37:14 +0200

    Camera Follow + Working Hook + Working Unity Version :sweat_smile: :sweat_smile: :sweat_smile: :sweat_smile:

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -8,7 +8,7 @@ public class CameraFollow : MonoBehaviour
     public float yOffset = 1f;
     public Transform target;
 
-   
+
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
